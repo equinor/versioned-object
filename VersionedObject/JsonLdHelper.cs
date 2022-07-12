51206a6599636278b14f6f1bec64191e0143fa0d
Dag Hovland <dhovl@equinor.com>
2022-07-12 10:20:30 +0200

    Formatting

--- VersionedObject/JsonLdHelper.cs
+++ VersionedObject/JsonLdHelper.cs
@@ -108,7 +108,7 @@ namespace VersionedObject
             return graph.GetHash();
         }
 
-       
+
         public static IRIReference GetIRIReference(this JToken jsonld) =>
             new(jsonld.SelectToken("@id")?.ToString() ?? throw new InvalidJsonLdException($"No @id field in object {jsonld}"));
 
